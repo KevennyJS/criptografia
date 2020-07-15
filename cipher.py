b08f55ce879491ac6d97490a8fdcaa68476fbb1c
kevenny JS <kevennykeke@gmail.com>
2020-07-15 10:10:07 -0300

    Update cipher.py

--- cipher.py
+++ cipher.py
@@ -1,6 +1,6 @@
 import sys
 ALPHABET = 'abcdefghijklmnopqrstuvwxyz'
-ROT = 13
+ROT = 3
 
 def cipher(message, dir):
     m = ''
@@ -23,11 +23,11 @@ def main():
     message = sys.argv[2].lower()
 
     if command == 'encrypt':
-        print encrypt(message)
+        print (encrypt(message))
     elif command == 'decrypt':
-        print decrypt(message)
+        print (decrypt(message))
     else:
-        print command + ' -> command not found'
+        print (command + ' -> command not found')
 
 if __name__ == '__main__':
     main()
